0d4b90d925d6123123234e65a46b6045cf876aee
kadavr95 <kadavr95@outlook.com>
2021-04-26 02:51:04 +0300

    Remove unused page method

--- homework02/ui/pages/unauthorized_page.py
+++ homework02/ui/pages/unauthorized_page.py
@@ -14,13 +14,3 @@ class UnauthorizedPage(BasePage):
         self.fill(password, self.locators.PASSWORD_FIELD_LOCATOR)
         self.click(self.locators.CREDENTIALS_SEND_BUTTON_LOCATOR)
         return AuthorizedPage(self.driver)
-
-    @allure.step('Going to {event}')
-    def go_to_events(self, event):
-        events_button = self.find(self.locators.EVENTS_BUTTON)
-        self.action_chains.move_to_element(events_button).perform()
-
-        event_locator = (self.locators.EVENTS_LINK_TEMPLATE[0],
-                         self.locators.EVENTS_LINK_TEMPLATE[1].format(event))
-        self.click(event_locator)
-        return AuthorizedPage(self.driver)
